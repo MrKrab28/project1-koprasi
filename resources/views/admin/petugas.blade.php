a7e35ca5b45734f87938afe743e09a10e29109a6
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-03-06 20:24:05 +0800

    hilangkan margin pada tombol tambah & pisahkan modal

--- resources/views/admin/petugas.blade.php
+++ resources/views/admin/petugas.blade.php
@@ -9,87 +9,8 @@
                         <div class="header-title">
                             <h4 class="mb-0">Daftar Petugas</h4>
                         </div>
-                        <div class="me-3">
-                            <button type="submit" class="btn btn-primary " data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">Tambah Data</button>
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Petugas
-                                            </h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <form action="{{ route('petugas-store') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" name="jenis" required>
-                                            <div class="modal-body">
-                                                <div class="mb-3">
-                                                    <label for="nama" class="form-label">Nama Petugas</label>
-                                                    <input class="form-control" name="nama" id="nama" required>
-
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="alamat" class="form-label">Alamat</label>
-                                                    <input type="text" class="form-control" id="alamat" name="alamat"
-                                                        required>
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="jabatan" class="form-label" id="jabatan">Jabatan</label>
-                                                    <input type="text" class="form-control" name="jabatan">
-
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="no_hp" class="form-label">No.Hp</label>
-                                                    <input type="number" class="form-control" id="no_hp" name="no_hp"
-                                                        required>
-                                                </div>
-                                                <div class="mb-3">
-                                                    <div class="form-check">
-                                                        <input class="form-check-input" type="radio" name="jk"
-                                                            value="L" id="jk">
-                                                        <label class="form-check-label" for="flexRadioDefault1">
-                                                            Laki - Laki
-                                                        </label>
-                                                    </div>
-                                                    <div class="form-check">
-                                                        <input class="form-check-input" type="radio" name="jk"
-                                                            value="P" id="flexRadioDefault2" checked>
-                                                        <label class="form-check-label" for="jk">
-                                                            Perempuan
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="email" class="form-label">Email</label>
-                                                    <input type="email" class="form-control" id="email" name="email"
-                                                        required>
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="password" class="form-label">Password</label>
-                                                    <input type="password" class="form-control" id="password"
-                                                        name="password" required>
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="tgl_bergabung" class="form-label">Tanggal
-                                                        Bergabung</label>
-                                                    <input type="date" class="form-control" id="tgl_bergabung"
-                                                        name="tgl_bergabung" required>
-                                                </div>
-                                                <div class="modal-footer">
-
-                                                    <button type="submit" class="btn btn-primary">Tambahkan</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">Tambah Data</button>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
@@ -97,7 +18,6 @@
                             <table id="table" class="table table-hover mt-5 w-100">
                                 <thead>
                                     <tr>
-
                                         <th>No</th>
                                         <th>Nama</th>
                                         <th>Alamat</th>
@@ -110,9 +30,6 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-
-
-
                                     @foreach ($petugas as $petugas)
                                         <tr>
                                             <td>{{ $loop->iteration }}</td>
@@ -156,6 +73,79 @@
         </div>
     </div>
 @endsection
+
+@push('modals')
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Petugas
+                    </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('petugas-store') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="jenis" required>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama Petugas</label>
+                            <input class="form-control" name="nama" id="nama" required>
+
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="alamat" class="form-label">Alamat</label>
+                            <input type="text" class="form-control" id="alamat" name="alamat" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="jabatan" class="form-label" id="jabatan">Jabatan</label>
+                            <input type="text" class="form-control" name="jabatan">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="no_hp" class="form-label">No.Hp</label>
+                            <input type="number" class="form-control" id="no_hp" name="no_hp" required>
+                        </div>
+                        <div class="mb-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="jk" value="L" id="jk">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Laki - Laki
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="jk" value="P"
+                                    id="flexRadioDefault2" checked>
+                                <label class="form-check-label" for="jk">
+                                    Perempuan
+                                </label>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tgl_bergabung" class="form-label">Tanggal
+                                Bergabung</label>
+                            <input type="date" class="form-control" id="tgl_bergabung" name="tgl_bergabung" required>
+                        </div>
+                        <div class="modal-footer">
+
+                            <button type="submit" class="btn btn-primary">Tambahkan</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endpush
+
 @push('styles')
     @include('includes.datatables.styles')
 @endpush
